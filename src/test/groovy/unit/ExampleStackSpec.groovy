dc999c0557a69d8c818a847ec2b166fd0a0105e2
Kevin Richards <krichards@avoco.eu>
2013-10-15 10:41:34 +0100

    Added simple unit test for Reference

---
+++ src/test/groovy/unit/ExampleStackSpec.groovy
@@ -0,0 +1,32 @@
+package unit
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class ExampleStackSpec extends Specification {
+
+    @Shared Stack stack
+
+    def setup() {
+        stack = new Stack()
+    }
+
+    def "empty stack size"() {
+        expect: stack.size() == 0
+    }
+
+    def "pop on empty stack throws exception"() {
+        when: stack.pop()
+        then: thrown(EmptyStackException)
+    }
+
+    def "push adds entry to which can be peeked"() {
+        when:
+        stack.push("elem")
+
+        then:
+        stack.size() == 1
+        stack.peek() == "elem"
+    }
+
+}
